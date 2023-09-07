7686c3dc139e66b559b5f614c3d9f9ee4c23b69e
Farhan Rizky <farhanrizkyyyy@gmail.com>
2023-09-07 13:55:34 +0700

    add relation on orders & payments

--- src/main/java/com/springboot/task6/model/Order.java
+++ src/main/java/com/springboot/task6/model/Order.java
@@ -1,6 +1,11 @@
 package com.springboot.task6.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.Date;
 
@@ -10,10 +15,64 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "total_amount")
     private Integer totalAmount;
     private String note;
+    @Column(name = " is_paid", columnDefinition = "BOOLEAN DEFAULT FALSE")
     private Boolean isPaid;
+    @Column(name = "created_at", columnDefinition = "DATE DEFAULT CURRENT_DATE")
     private Date createdAt;
+    @Column(name = "updated_at", columnDefinition = "DATE DEFAULT NULL")
     private Date updatedAt;
+    @Column(name = "deleted_at", columnDefinition = "DATE DEFAULT NULL")
     private Date deletedAt;
+    @OneToOne(mappedBy = "order")
+    private Payment payment;
+
+    public Order(Integer totalAmount, String note, Boolean isPaid) {
+        this.totalAmount = totalAmount;
+        this.note = note;
+        this.isPaid = isPaid;
+        this.payment = null;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Integer getTotalAmount() {
+        return this.totalAmount;
+    }
+
+    public String getNote() {
+        return this.note;
+    }
+
+    public Boolean getIsPaid() {
+        return this.isPaid;
+    }
+
+    public void setIsPaid(Boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return this.deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
--- src/main/java/com/springboot/task6/model/Payment.java
+++ src/main/java/com/springboot/task6/model/Payment.java
@@ -1,6 +1,14 @@
 package com.springboot.task6.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import java.util.Date;
 
 @Entity
 @Table(name = "payments")
@@ -8,7 +16,60 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "total_paid")
     private Integer totalPaid;
     private Integer discount;
     private Integer change;
+    @Column(name = "created_at", columnDefinition = "DATE DEFAULT CURRENT_DATE")
+    private Date createdAt;
+    @Column(name = "updated_at", columnDefinition = "DATE DEFAULT NULL")
+    private Date updatedAt;
+    @Column(name = "deleted_at", columnDefinition = "DATE DEFAULT NULL")
+    private Date deletedAt;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Payment(Integer totalPaid, Integer discount, Integer change) {
+        this.totalPaid = totalPaid;
+        this.discount = discount;
+        this.change = change;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Integer getTotalPaid() {
+        return this.totalPaid;
+    }
+
+    public Integer getDiscount() {
+        return this.discount;
+    }
+
+    public Integer getChange() {
+        return this.change;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return this.deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
