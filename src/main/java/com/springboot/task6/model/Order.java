e5f0b872de178dc81f3c34fec17db926c5b2db76
Tharissa Najwa <tharissaazz@gmail.com>
2023-09-07 13:32:15 +0700

    add: relation on employee, order and product

--- src/main/java/com/springboot/task6/model/Order.java
+++ src/main/java/com/springboot/task6/model/Order.java
@@ -1,8 +1,20 @@
 package com.springboot.task6.model;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -10,10 +22,31 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "table_id", referencedColumnName = "id")
+    private TableOrder table;
+
+//    @JsonIgnore
+//    @OneToMany(mappedBy = "order")
+//    private List<OrderProduct> orderProducts;
+
+    @Column(name = "total_amount")
     private Integer totalAmount;
-    private String note;
+    private String notes;
+    @Column(name = "is_paid")
     private Boolean isPaid;
+    @Column(name = "created_at", columnDefinition = "DATE DEFAULT CURRENT_DATE")
     private Date createdAt;
+    @Column(name = "updated_at", columnDefinition = "DATE DEFAULT NULL")
     private Date updatedAt;
+    @Column(name = "deleted_at", columnDefinition = "DATE DEFAULT NULL")
     private Date deletedAt;
 }
